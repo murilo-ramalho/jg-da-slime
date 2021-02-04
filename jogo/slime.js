e5fe2bb1395d500bc87581dbc1711a8fe8f02a9a
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2021-02-04 15:57:27 -0300

    Update slime.js

--- jogo/slime.js
+++ jogo/slime.js
@@ -1,5 +1,5 @@
 //variaveis do jg
-const scene = new THREE.scene();
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
